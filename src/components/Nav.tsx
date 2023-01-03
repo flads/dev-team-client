61080d9bb6e206ca4e5f6c146ae3a71f688da00b
flads <flads.me@gmail.com>
2023-01-02 21:07:36 -0300

    fix: removing link from nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,12 +6,11 @@ function Nav() {
 
   return (
     <>
-      <nav
-        className="flex justify-center sm:justify-start gap-x-2 items-center text-blue-600 font-medium text-base pl-4 cursor-pointer"
-        onClick={() => navigate('/')}
-      >
-        <HomeIcon />
-        <h1 className="hidden sm:inline">Dev Team</h1>
+      <nav className="flex justify-center sm:justify-start gap-x-2 items-center text-blue-600 font-medium text-base pl-4 cursor-pointer">
+        <HomeIcon onClick={() => navigate('/')} />
+        <h1 className="hidden sm:inline" onClick={() => navigate('/')}>
+          Dev Team
+        </h1>
       </nav>
     </>
   );
--- src/components/home/HomeItem.tsx
+++ src/components/home/HomeItem.tsx
@@ -1,9 +1,12 @@
 import { HomeItem as HomeItemInterface } from '../../interfaces/home.interface';
 import ChevronDoubleRightIcon from '../icons/ChevronDoubleRight';
 
-function HomeItem({ title }: HomeItemInterface) {
+function HomeItem({ title, onClick }: HomeItemInterface) {
   return (
-    <div className="flex justify-between bg-gradient-to-r from-blue-500 to-blue-400 w-full text-sm sm:text-base sm:w-2/4 mt-2 sm:m-2 p-2 sm:p-4 rounded text-white font-medium cursor-pointer hover:underline">
+    <div
+      className="flex justify-between bg-gradient-to-r from-blue-500 to-blue-400 w-full text-sm sm:text-base sm:w-2/4 mt-2 sm:m-2 p-2 sm:p-4 rounded text-white font-medium cursor-pointer hover:underline"
+      onClick={onClick}
+    >
       <p>{title}</p>
       <ChevronDoubleRightIcon />
     </div>
