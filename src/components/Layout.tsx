c54ae6b5bd216a0dcd3874ef0875b35d13d6bfab
flads <flads.me@gmail.com>
2023-01-02 21:08:03 -0300

    refactor: changing layout background color

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,9 @@
-import { LayoutProps } from '../interfaces/layout.interface';
+import { LayoutProps } from '../interfaces/components/layout.interface';
 import Nav from '../components/Nav';
 
 function Layout({ children }: LayoutProps) {
   return (
-    <div className="bg-blue-50 min-h-screen p-2 pt-8 sm:p-8">
+    <div className="bg-gray-100 min-h-screen p-2 pt-8 sm:p-8">
       <div className="max-w-screen-lg mx-auto">
         <Nav />
         {children}
