0b4018f96e83af5c16cf35452021a99dde726fbc
flads <flads.me@gmail.com>
2023-01-02 21:11:12 -0300

    feature: creating button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+import { ButtonProps } from '../interfaces/components/button.interface';
+
+function Button({ children, onClick }: ButtonProps) {
+  return (
+    <button
+      type="button"
+      className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-100"
+      id="menu-button"
+      aria-expanded="true"
+      aria-haspopup="true"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
