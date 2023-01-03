1bd934a28b708db7ef243351f50fb6a848d367ac
flads <flads.me@gmail.com>
2023-01-02 22:38:44 -0300

    feature: install and configure internationalization

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,31 @@
+import { I18nextProvider } from 'react-i18next';
+import App from './App';
+import common_ptBR from './translations/ptBR/common.json';
+import i18next from 'i18next';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: 'ptBR',
+  resources: {
+    ptBR: {
+      common: common_ptBR,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
 
