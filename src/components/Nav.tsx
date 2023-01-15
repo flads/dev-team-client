044f3d1abefd8d443999dffbfd8358646c77c4a8
flads <flads.me@gmail.com>
2023-01-15 14:21:35 -0300

    refactor(nav-component): improving the click

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,11 +6,14 @@ function Nav() {
 
   return (
     <>
-      <nav className="flex justify-center sm:justify-start gap-x-2 items-center text-blue-600 font-medium text-base pl-4 cursor-pointer">
-        <HomeIcon onClick={() => navigate('/')} />
-        <h1 className="hidden sm:inline" onClick={() => navigate('/')}>
-          Dev Team
-        </h1>
+      <nav className="flex justify-center sm:justify-start">
+        <div
+          className="p-2 flex justify-center sm:justify-start items-center gap-x-2 text-blue-600 font-medium text-base pl-4 cursor-pointer"
+          onClick={() => navigate('/')}
+        >
+          <HomeIcon />
+          <h1 className="hidden sm:inline">Dev Team</h1>
+        </div>
       </nav>
     </>
   );
